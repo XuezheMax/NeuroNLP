4e280361ea156381cec75896cb8e1ed0a84db365
Max Ma <xuezhe.ma@gmail.com>
2016-11-15 10:31:34 -0500

    minor changes

--- neuronlp/utils.py
+++ neuronlp/utils.py
@@ -7,6 +7,8 @@ from gensim.models.word2vec import Word2Vec
 import gzip
 import theano
 
+from .io import data_utils
+
 def get_logger(name, level=logging.INFO, handler=sys.stdout,
                formatter='%(asctime)s - %(name)s - %(levelname)s - %(message)s'):
     logger = logging.getLogger(name)
@@ -20,7 +22,7 @@ def get_logger(name, level=logging.INFO, handler=sys.stdout,
     return logger
 
 
-def load_word_embedding_dict(embedding, embedding_path, embedd_dim=100):
+def load_word_embedding_dict(embedding, embedding_path, normalize_digits=True):
     """
     load word embeddings from file
     :param embedding:
@@ -49,7 +51,8 @@ def load_word_embedding_dict(embedding, embedding_path, embedd_dim=100):
                     assert (embedd_dim + 1 == len(tokens))
                 embedd = np.empty([1, embedd_dim], dtype=theano.config.floatX)
                 embedd[:] = tokens[1:]
-                embedd_dict[tokens[0]] = embedd
+                word = data_utils.DIGIT_RE.sub(b"0", tokens[0]) if normalize_digits else tokens[0]
+                embedd_dict[word] = embedd
         return embedd_dict, embedd_dim, True
     elif embedding == 'senna':
         # loading Senna
@@ -68,7 +71,8 @@ def load_word_embedding_dict(embedding, embedding_path, embedd_dim=100):
                     assert (embedd_dim + 1 == len(tokens))
                 embedd = np.empty([1, embedd_dim], dtype=theano.config.floatX)
                 embedd[:] = tokens[1:]
-                embedd_dict[tokens[0]] = embedd
+                word = data_utils.DIGIT_RE.sub(b"0", tokens[0]) if normalize_digits else tokens[0]
+                embedd_dict[word] = embedd
         return embedd_dict, embedd_dim, True
     else:
         raise ValueError("embedding should choose from [word2vec, senna]")
