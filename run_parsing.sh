0f0a8dec3bf25077e097374d73c6839268ff8e3d
Max Ma <xuezhe.ma@gmail.com>
2016-11-17 01:16:42 -0500

    shell

--- run_parsing.sh
+++ run_parsing.sh
@@ -1,6 +1,7 @@
-THEANO_FLAGS='floatX=float32' python parsing.py --num_epochs 500 --batch_size 100 --num_units 100 --num_filters 30 --normalize_digits \
- --opt adam --learning_rate 0.001 --decay_rate 0.1 --schedule 200 300 400 --grad_clipping 5 --regular none --dropout 0.0 --delta 0.0 \
+THEANO_FLAGS='floatX=float32' python parsing.py --num_epochs 200 --batch_size 200 --num_units 300 --num_filters 50 --normalize_digits --pos \
+ --opt adam --learning_rate 0.002 --decay_rate 0.75 --schedule 2000 --grad_clipping 5.0 --regular none --dropout 0.25 \
  --punctuation ", . \`\` : ''" \
  --train "data/PTB3.0/PTB3.0-Stanford_dep/ptb3.0-stanford.auto.cpos.train.conll" \
  --dev "data/PTB3.0/PTB3.0-Stanford_dep/ptb3.0-stanford.auto.cpos.dev.conll" \
- --test "data/PTB3.0/PTB3.0-Stanford_dep/ptb3.0-stanford.auto.cpos.test.conll" 
+ --test "data/PTB3.0/PTB3.0-Stanford_dep/ptb3.0-stanford.auto.cpos.test.conll" \
+ --tmp "tmp"
