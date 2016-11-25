45bb82a3d789cef1116ddc3adbb780973b266c1e
Max Ma <xuezhe.ma@gmail.com>
2016-11-25 02:08:50 -0500

    elu

--- parsing.py
+++ parsing.py
@@ -194,7 +194,7 @@ def build_network(word_var, char_var, pos_var, mask_var, word_alphabet, char_alp
                                                 num_leading_axes=2, name='dense%d' % d)
         bi_lstm_cnn = lasagne.layers.DropoutLayer(bi_lstm_cnn, p=0.33, shared_axes=(1,))
 
-    bi_lstm_cnn = lasagne.layers.DenseLayer(bi_lstm_cnn, 100, nonlinearity=nonlinearities.linear,
+    bi_lstm_cnn = lasagne.layers.DenseLayer(bi_lstm_cnn, 100, nonlinearity=nonlinearities.elu,
                                             num_leading_axes=2, name='dense%d' % mlp)
 
     return TreeBiAffineCRFLayer(bi_lstm_cnn, num_types, mask_input=mask, name='crf')
