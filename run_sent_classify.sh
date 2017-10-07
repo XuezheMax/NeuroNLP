833c894cccd34790e3f9542137f60ab241bb1948
Max Ma <xuezhe.ma@gmail.com>
2017-10-07 16:08:34 -0400

    run_sent

--- run_sent_classify.sh
+++ run_sent_classify.sh
@@ -1,2 +1,2 @@
-THEANO_FLAGS='floatX=float32' python sent_classify.py --architec maxru --num_epochs 100 --batch_size 16 --num_units 64 --num_time_units 32 \
+THEANO_FLAGS='floatX=float32' python sent_classify.py --architec sgru --num_epochs 100 --batch_size 16 --num_units 64 \
  --learning_rate 0.001 --decay_rate 0.1 --schedule 30 70 --grad_clipping 0 
